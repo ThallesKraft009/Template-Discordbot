3261b3b05c6d418284174497c222dccac1e0d7d6
ThallesKraft <99352499+ThallesKraft009@users.noreply.github.com>
2022-11-05 14:53:47 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const config = require('./config.json')
+const fs = require('fs')
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds, 
+		GatewayIntentBits.GuildMessages, 
+		GatewayIntentBits.GuildPresences, 
+    GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildMessageReactions, 
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent
+	], 
+	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
+});
+
+
+client.commands = new Collection()
+client.aliases = new Collection()
+client.prefix = config.prefix
+client.config = require('./config.json');
+client.slashCommands = new Collection();
+module.exports = client;
+client.categories = fs.readdirSync("./commands/");
+
+["command", "events", "slashCommand"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+
+
+client.login(config.token)
+
